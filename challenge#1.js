3afc72502044cebaca38f12c741985119501d088
Grace Dzinotyiwei <117931035+GraceDzinotyiwei@users.noreply.github.com>
2024-11-07 13:40:53 +0200

    Create challenge#1.js

---
+++ challenge#1.js
@@ -0,0 +1,32 @@
+
+//lesson 1 
+//Print numbers from 1 to 10
+//////////////////////////////////////////////////////////
+
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+console.log("6")
+console.log("7")
+console.log("8")
+console.log("9")
+console.log("10")
+
+//////////////////////////////////////////////////////////
+
+
+for (var i=1; i<11;i++){
+    console.log(i);
+}
+
+//////////////////////////////////////////////////////
+
+var i=1
+while (i<11) {
+    console.log(i)
+    i++
+}
+
+*/
