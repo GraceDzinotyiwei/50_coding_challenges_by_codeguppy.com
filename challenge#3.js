ed61f2053995d474087853904d89e4bf7f37e02f
Grace Dzinotyiwei <117931035+GraceDzinotyiwei@users.noreply.github.com>
2024-11-08 10:19:43 +0200

    Create challenge#3.js

---
+++ challenge#3.js
@@ -0,0 +1,21 @@
+//challenge 3
+//Print the multiplication table with 7
+/*7,14,21,28....
+
+///////////////////////////////////////////////////
+
+for(var i=0; i<13;i++){
+    var a=  7 * i;
+    console.log("7 x ", i, " = ", a);
+
+}
+
+
+//////////////////////////////////////////////////
+var i = 0
+while(i<13){
+    var a = i*7;
+    console.log("7 x ", i, " = ", a);
+    i++;
+}
+*/
