f377b340a1cb73cd283b71a18f0bd2a6bf19e36a
Grace Dzinotyiwei <117931035+GraceDzinotyiwei@users.noreply.github.com>
2024-11-11 11:37:45 +0200

    Create challenge#9.js

---
+++ challenge#9.js
@@ -0,0 +1,9 @@
+//challenge 9
+//Create a function that will convert from Fahrenheit to Celsius
+//c =(f-32)x 5/9
+
+function fahrenheitToCelsius(f){
+    return (f-32) * (5/9)
+}
+var c = fahrenheitToCelsius(50)
+console.log(c)
